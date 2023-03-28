daf74ac62454c7633fc90fb26e927d423b6b8ead
mohmmad albaba <albabam9@gmail.com>
2023-03-28 10:32:54 +0300

    edit test data

--- GUI_Web_Cucumber/src/test/java/shaft/Base/TestData.java
+++ GUI_Web_Cucumber/src/test/java/shaft/Base/TestData.java
@@ -6,33 +6,33 @@ public class TestData {
     static Faker faker = new Faker();
     public static String TestRole=faker.random().hex();
     public class ManagerData{
-        static String name= faker.random().hex();
+        public static String name= faker.random().hex();
 
       //  static String name="ManagerTest";
-      static String email= faker.internet().emailAddress();
+        public static String email= faker.internet().emailAddress();
 
         //static String email="manager@gmail.com";
-         static String password="123456";
-         static String nameRole="Super admin";
+        public static String password="123456";
+        public static String nameRole="Super admin";
     }
 
     public class UserData{
-         static String numberUser="2050";
-         static String firstName="test";
-         static String middleName="test";
-         static String lastName="test";
-         static String email="user@gmail.com";
-         static String telephone="082565290";
-         static String mobile="+966 56 464 6577";
-         static String password="123456";
-         static String country="";
-         static String city="";
-         static String company="";
-         static String branch="";
-         static String department="";
-         static String position="";
-         static String groups="";
-         static String successMessage="شكراً لك. لقد تم الإرسال بنجاح.";
+        public static String numberUser="2050";
+        public static String firstName="test";
+        public static String middleName="test";
+        public static String lastName="test";
+        public  static String email="user@gmail.com";
+        public static String telephone="082565290";
+        public static String mobile="+966 56 464 6577";
+        public static String password="123456";
+        public  static String country="";
+        public static String city="";
+        public static String company="";
+        public static String branch="";
+        public static String department="";
+        public  static String position="";
+        public static String groups="";
+        public static String successMessage="شكراً لك. لقد تم الإرسال بنجاح.";
     }
 }
 
